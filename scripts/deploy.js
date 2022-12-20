0813474a85d8ee02dbc9edf0ad3b179d7b7f20c7
superdev <superdev0241002@gmail.com>
2022-12-20 06:46:44 -0800

    A fix: deploy the erc20 token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,11 @@ async function main() {
   // We get the contract to deploy
   const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
   const simpleStorage = await SimpleStorage.deploy(789);
-  const Token = await ethers.getContractFactory("Token");
+  const Token = await ethers.getContractFactory("NDToken");
   const token = await Token.deploy("ERC20 Token","ERC20-Token")
   // NOTE: All Contracts have an associated address
   console.log("SimpleStorage deployed to:", simpleStorage.address);
+  console.log("ERC20 deployed to:", token.address);
 }
 
 main()
